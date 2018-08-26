2e78a49f8e8e90d807960ecd7379f8c6fa6358de
rohit <rohityadav2@outlook.com>
2018-08-26 22:59:26 +0530

    Import modules

--- we_rate_dogs.py
+++ we_rate_dogs.py
@@ -6,3 +6,19 @@
 # 
 # WeRateDogs is a Twitter account that rates people's dogs with a humorous comment about the dog. These ratings almost always have a denominator of 10. The numerators, though? Almost always greater than 10. 11/10, 12/10, 13/10, etc. Why? Because "they're good dogs Brent." WeRateDogs has over 4 million followers and has received international media coverage.
 # I will do a wrangling, analyzing and visualization of the tweet data of the WeRateDogs.
+
+# In[1]:
+
+
+# Import modules
+import json
+import os
+import time
+import pandas as pd
+import numpy as np
+import requests
+import tweepy
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud,STOPWORDS
+
