a0d17c0855d37cffa728fe07c68e6baf8b522c6b
rohit <rohityadav2@outlook.com>
2018-08-26 22:56:22 +0530

    Doc: Introduction

--- we_rate_dogs.py
+++ we_rate_dogs.py
@@ -1,2 +1,8 @@
 
 # coding: utf-8
+
+# 
+# # We Rate Dogs
+# 
+# WeRateDogs is a Twitter account that rates people's dogs with a humorous comment about the dog. These ratings almost always have a denominator of 10. The numerators, though? Almost always greater than 10. 11/10, 12/10, 13/10, etc. Why? Because "they're good dogs Brent." WeRateDogs has over 4 million followers and has received international media coverage.
+# I will do a wrangling, analyzing and visualization of the tweet data of the WeRateDogs.
