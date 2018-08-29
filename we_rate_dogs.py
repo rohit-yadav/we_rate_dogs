39b49009ede0dfdd856a47516961cc3c00a90a5d
rohit <rohityadav2@outlook.com>
2018-08-29 11:59:03 +0530

    Refactor: Don't connect to twitter if file exists
    This will allow to work without the twitter key and secret codes

--- we_rate_dogs.py
+++ we_rate_dogs.py
@@ -34,10 +34,13 @@ access_secret = 'HIDDEN'
 
 # Function to get twitter connection
 def twitter_connection(consumer_key, consumer_secret, access_token, access_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
+    if os.path.isfile("data/tweet_json.txt"):
+        return "File already exists"
+    else:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_secret)
 
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     
     return api
 
